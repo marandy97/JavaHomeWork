d14cd9418c1cf3cbb6f2bcb4791cef2436d4e003
Marandy <marandyahome@gmail.com>
2021-10-13 18:29:07 +0300

    init commit

---
+++ src/com/pb/marandy/hw2/Interval.java
@@ -0,0 +1,22 @@
+package com.pb.marandy.hw2;
+import java.util.Scanner;
+public class Interval {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        int xs = in.nextInt();
+        if (xs > 0 && xs <= 14) {
+            System.out.print("Число в промежутке от 0 до 14");
+        }
+        else if (xs >= 15 && xs <= 35) {
+            System.out.print("Число в промежутке от 15 до 35");
+        }
+        else if (xs >= 36 && xs <= 50) {
+            System.out.print("Число в промежутке от 36 до 50");
+        } else {
+            System.out.print("Число в промежутке больше 50");
+        }
+
+    }
+}
