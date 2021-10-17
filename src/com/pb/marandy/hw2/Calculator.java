1146392500503d3f87a1015fbaefe9a1f9c3a806
Marandy <marandyahome@gmail.com>
2021-10-17 13:21:01 +0300

    init commit

--- src/com/pb/marandy/hw2/Calculator.java
+++ src/com/pb/marandy/hw2/Calculator.java
@@ -1,65 +1,28 @@
 package com.pb.marandy.hw2;
-
 import java.util.Scanner;
+class Calculator {
+    public static Scanner in = new Scanner(System.in);
 
-public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int operand1 = nextInt();
-        int operand2 = nextInt();
-        char operation = getOperation();
-        int result = calc(operand1,operand2,operation);
-        System.out.println("Результат операции: "+result);
-    }
-
-    public static int nextInt(){
-        System.out.println("Введите число:");
-        int operand;
-        if(scanner.hasNextInt()){
-            operand = scanner.nextInt();
-        } else {
-            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            operand = nextInt();
+        int operand1, operand2;
+        String sign;
+        {
+            System.out.println("Введите число:");
+            operand1 = in.nextInt();
+            System.out.println("Введите операцию:");
+            sign = in.next();
+            System.out.println("Введите второе число:");
+            operand2 = in.nextInt();
+            if (sign.equals("+")) {
+                System.out.println(operand1 + operand2);
+            } else if (sign.equals("-")) {
+                System.out.println(operand1 - operand2);
+            } else if (sign.equals("*")) {
+                System.out.println(operand1 * operand2);
+            } else if (sign.equals("/") && operand2 != 0) {
+                System.out.println(operand1 / operand2);
+            } else System.out.println("Введена не верная операция, либо попытка поделить на ноль");
         }
-        return operand;
-    }
-
-    public static char getOperation(){
-        System.out.println("Введите операцию:");
-        char operation;
-        if(scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            operation = getOperation();
-        }
-        return operation;
-    }
-
-    public static int calc(int operand1, int operand2, char operation){
-        int result;
-        switch (operation){
-            case '+':
-                result = operand1+operand2;
-                break;
-            case '-':
-                result = operand1-operand2;
-                break;
-            case '*':
-                result = operand1*operand2;
-                break;
-            case '/':
-                result = operand1/operand2;
-                break;
-            default:
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(operand1, operand2, getOperation());//рекурсия
-
-        }
-
-        return result;
     }
 }
